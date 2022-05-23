e5deb3df012d3d231572021f57b9656961fbae6d
River Smith <22485304+r-tae@users.noreply.github.com>
2022-05-23 12:52:09 +1000

    Multiple updates; theme changes; nvim plugins
    Switched to catppuccin theming for everything (almost)
    Added LuaSnip snippets (copied from example repo, should remove some
    vanilla JS ones)
    Added kitty config
    Tmux theming
    Tmux now starts automatically

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,8 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-if status is-interactive
-    if not set -q TMUX
-        exec tmux
-    end
-    # Commands to run in interactive sessions can go here
-end
-
 set fish_greeting ""
 
-set -gx TERM xterm-256color
+set -gx TERM screen-256color
 
 alias ls "ls -p -G"
 alias ls "exa --all"
@@ -23,3 +16,17 @@ command -qv nvim && alias vim nvim
 set -gx EDITOR nvim
 
 starship init fish | source
+
+set -gx SHELL $(which fish)
+set -gx COLORTERM = truecolor
+
+# Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
+# only run in interactive (not automated SSH for example)
+if status is-interactive
+# don't nest inside another tmux
+and not set -q TMUX
+  # Adapted from https://unix.stackexchange.com/a/176885/347104
+  # Create session 'main' or attach to 'main' if already exists.
+  exec tmux new-session -A -s main
+	# using `exec' is a tradeoff; it means you don't have to `exit' twice just to close a terminal window but if tmux has errors, fish will be unusable.
+end
